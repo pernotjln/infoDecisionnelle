abe915068901e4b52bd19c07526ea1c51fbec0e7
peyet <peyet@peyet-debian>
2015-06-01 14:15:09 +0200

    ajout du main

---
+++ infoDecisionnelle/src/main/java/com/pernotpeyetsainthillier/infodecisionnelle/Decideur.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pernotpeyetsainthillier.infodecisionnelle;
+
+/**
+ *
+ * @author peyet
+ */
+public class Decideur {
+	public static void main(String args[]) {
+		System.out.println("Hello world !");
+	}
+}
