edf5b8d8230727e33a25f373183458021d7293c2
peyet <peyet@peyet-debian>
2015-06-01 18:36:04 +0200

    refacto validateur

---
+++ infoDecisionnelle/src/main/java/com/pernotpeyetsainthillier/infodecisionnelle/Decideur.java
@@ -0,0 +1,20 @@
+package com.pernotpeyetsainthillier.infodecisionnelle;
+
+import java.io.File;
+
+import weka.classifiers.bayes.NaiveBayesUpdateable;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+
+/**
+ * 
+ * @author peyet pernot sainthillier
+ */
+public class Decideur {
+	public static void main(String args[]) throws Throwable {
+		
+		new FenetrePrincipale();
+		
+	}
+}
---
+++ infoDecisionnelle/src/main/java/com/pernotpeyetsainthillier/infodecisionnelle/Validateur.java
@@ -0,0 +1,82 @@
+package com.pernotpeyetsainthillier.infodecisionnelle;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import weka.classifiers.bayes.NaiveBayesUpdateable;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+
+/**
+ * 
+ * @author peyet pernot sainthillier
+ */
+public class Validateur {
+	private static Validateur instance = null;
+	
+	/*
+	 * Constructeur privé du singleton
+	 */
+	private Validateur() {
+		try {
+			// load data
+			ArffLoader loader = new ArffLoader();
+			loader.setFile(new File("./dataSimple.arff"));
+			Instances structure = loader.getStructure();
+			structure.setClassIndex(structure.numAttributes() - 1);
+	
+			// train NaiveBayes
+			NaiveBayesUpdateable nb = new NaiveBayesUpdateable();
+			nb.buildClassifier(structure);
+			Instance current;
+			while ((current = loader.getNextInstance(structure)) != null) {
+				nb.updateClassifier(current);
+			}
+	
+			// object
+			Instance person = new Instance(8);
+			person.setValue(structure.attribute(0), ">=200");
+			person.setValue(structure.attribute(1), "all paid");
+			person.setValue(structure.attribute(2), "radio/tv");
+			person.setValue(structure.attribute(3), 1169.0);
+			person.setValue(structure.attribute(4), "<100");
+			person.setValue(structure.attribute(5), "bank");
+			person.setValue(structure.attribute(6), "skilled");
+	
+			person.setDataset(structure);
+			nb.updateClassifier(person);
+	
+			// result
+			System.out.println(nb);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	/*
+	 * Récupération de l'instance unique
+	 */
+	public static Validateur getInstance() {
+		if (Validateur.instance == null) {
+			Validateur.instance = new Validateur();
+		}
+		
+		return Validateur.instance;
+	}
+	
+	/*
+	 * Permet d'évaluer une personne (à implémenter !)
+	 */
+	public Map<String, Double> evaluer(Person personne) {
+		HashMap<String, Double> results = new HashMap<String, Double>();
+		
+		results.put("bon", 60.5);
+		results.put("mauvais", 39.5);
+		
+		return results;
+	}
+}
